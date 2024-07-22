80ed4f0029334fa6e798e85856fdaf7063b9e597
Dilan <dilandarren1@gmail.com>
2024-07-22 22:58:23 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import express, { text } from 'express';
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    res.render('inicio');
+}); 
+
+router.get('/nosotros', (req, res) => { //req - lo que enviamos : res - lo que express nos responde
+    const viajes = 'Viaje a Alemania';
+
+    res.render('nosotros', {
+        viajes
+    });
+}); 
+
+
+export default router;
+
