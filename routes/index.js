421018033447c84722e47f899d0c8cdfe2096396
Dilan <dilandarren1@gmail.com>
2024-07-26 17:38:26 +0200

    add testimonial

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,17 @@ import {
     paginaDetalleViaje
 } from '../controllers/paginaController.js';
 
+import{
+
+    guardarTestimonial
+
+} from '../controllers/testimonialControllers.js';
+
 const router = express.Router();
 
 
 router.get('/', paginaInicio);
+
 router.get('/nosotros', paginaNosotros); 
 
 router.get('/viajes', paginaViajes); 
@@ -18,6 +25,10 @@ router.get('/viajes', paginaViajes);
 router.get('/viajes/:slug', paginaDetalleViaje); 
 
 router.get('/testimoniales', paginaTestimoniales); 
+
+router.post('/testimoniales', guardarTestimonial); 
+
+
 
 
 export default router;
